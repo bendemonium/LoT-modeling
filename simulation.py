88afd60680308bf67bdafcb318667c11cf5ae2ff
Ridhi Bandaru <ridhib2422@gmail.com>
2025-02-13 21:08:14 -0500

    added similarity

--- simulation.py
+++ simulation.py
@@ -29,7 +29,7 @@ class StringGenerator:
             self.pairs = list(itertools.combinations(self.sequence_list, 2))
             self.levenshtein = np.array([Levenshtein.distance(s1, s2) for s1, s2 in self.pairs])
             self.lcs = np.array([self.longest_common_substring(s1, s2) for s1, s2 in self.pairs])
-            self.lcs_lengths = np.array([len(lcs) for lcs in self.lcs])
+            self.lcs_len = np.array([len(lcs) for lcs in self.lcs])
 
         @staticmethod
         def longest_common_substring(s1, s2):
@@ -55,44 +55,57 @@ class StringGenerator:
             self.n_rounds = n_rounds
             self.levenshtein = None
             self.lcs = None
-            self.lcs_length = None
+            self.lcs_len = None
+            self.lcs_len_norm = None
             
         def run_simulation(self):
             all_levenshtein = []
             all_lcs = []
             all_lcs_length = []
+            len_seq = self.parent.len_seq
             
             for _ in range(self.n_rounds):
                 round_instance = self.parent.Round(self.parent.symbol_sets, self.parent.len_seq, self.n_seq)
                 all_levenshtein.extend(round_instance.levenshtein)
                 all_lcs.extend(round_instance.lcs)
-                all_lcs_length.extend(round_instance.lcs_lengths)
+                all_lcs_length.extend(round_instance.lcs_len)
 
             # Store the results as attributes of the instance
             self.levenshtein = np.array(all_levenshtein)
+            self.similarity = self.levenshtein / len_seq
             self.lcs = np.array(all_lcs)
-            self.lcs_lengths = np.array(all_lcs_length)
+            self.lcs_len = np.array(all_lcs_length)
+            self.lcs_len_norm = self.lcs_len / len_seq
 
-def plot_simulation(metric, type):
+            return (self.levenshtein, self.lcs_len, self.similarity, self.lcs_len_norm, len_seq)
+            
+
+def plot_simulation(metrics):
     sns.set(style="whitegrid")
-    plt.figure(figsize=(10, 6))
-    ax = sns.histplot(metric, discrete=True, bins=int(max(metric)-min(metric)+1))  # Ensures discrete x-axis
+    fig, axes = plt.subplots(2, 2, figsize=(12, 10))
+    fig.suptitle("Metric Distributions", fontsize=16)
+    
+    metric1, metric2, metric3, metric4, len_seq = metrics
+    titles = ["Levenshtein Distance", "Longest Common Substring", "Similarity", "Longest Common Substring (normalized)"]
 
-    # Set x-axis ticks to integers
-    ax.set_xticks(range(int(min(metric)), int(max(metric)) + 1))
-    
-    # Display count on top of each bar
-    for patch in ax.patches:
-        height = patch.get_height()
-        ax.annotate(f'{int(height)}', 
-                    (patch.get_x() + patch.get_width() / 2., height),
-                    ha='center', va='center', fontsize=12, color='black', xytext=(0, 5), textcoords='offset points')
-    
-    if type == 'ED':
-        plt.xlabel("Levenshtein Distance", fontsize=14)
-    elif type == 'LCS':
-        plt.xlabel("Length of the Longest Common Substring", fontsize=14)
-    
-    plt.title("Distribution", fontsize=14)
+    for i, (metric, title) in enumerate(zip([metric1, metric2, metric3, metric4], titles)):
+        ax = axes[i // 2, i % 2]
+
+        if i < 2:  # Top two plots (discrete bins)
+            sns.histplot(metric, discrete=True, bins=int(max(metric) - min(metric) + 1), ax=ax)
+        else:  # Bottom two plots (len_seq bins)
+            sns.histplot(metric, bins=len_seq, ax=ax)
+
+        ax.set_title(title, fontsize=14)
+        ax.set_xlabel("Value", fontsize=12)
+
+        # Display count on top of each bar
+        for patch in ax.patches:
+            height = patch.get_height()
+            ax.annotate(f'{int(height)}', 
+                        (patch.get_x() + patch.get_width() / 2., height),
+                        ha='center', va='center', fontsize=10, color='black', xytext=(0, 5), textcoords='offset points')
+
+    plt.tight_layout(rect=[0, 0, 1, 0.96])
     plt.show()
 
